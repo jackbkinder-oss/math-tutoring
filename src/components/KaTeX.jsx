a11c484e93ce6d5e1529b39d67b129418953336c
Jack Kinder <jackkinder@big-dawg-money-business-63.local>
2026-04-08 12:54:38 +1000

    Add KaTeX component

---
+++ src/components/KaTeX.jsx
@@ -0,0 +1,17 @@
+import katex from 'katex';
+
+export function InlineMath({ math }) {
+  const html = katex.renderToString(math, {
+    throwOnError: false,
+    displayMode: false,
+  });
+  return <span dangerouslySetInnerHTML={{ __html: html }} />;
+}
+
+export function BlockMath({ math }) {
+  const html = katex.renderToString(math, {
+    throwOnError: false,
+    displayMode: true,
+  });
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+}
